2af5126d374bb600c5636e936a0744da8df2c479
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-05 21:07:19 +0100

    chore: Adding packages to depends

--- tests/testthat/test-access_img.R
+++ tests/testthat/test-access_img.R
@@ -7,6 +7,7 @@ plot(pressure)
 test_that("errors as expected", {
   # errors on null alt text
   expect_error(access_img(), "Please include alt text.")
+  expect_error(access_img(alt = ""), "Please include alt text.")
   
 })
 
