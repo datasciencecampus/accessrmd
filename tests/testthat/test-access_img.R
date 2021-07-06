0b10988d02cc7fc8170788298991de94d096d42d
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-06 22:01:06 +0100

    refactor: Upstream plot dependencies in access_head tests

--- tests/testthat/test-access_img.R
+++ tests/testthat/test-access_img.R
@@ -1,24 +1,25 @@
 # tests -------------------------------------------------------------------
-test_that("errors as expected", {
-  # errors on null image
+test_that("null img error", {
   expect_error(access_img(alt = "good alt"), "No img found.")
+  
+})
+
+# produce plot
+ggplot(mpg, aes(displ, hwy)) + geom_point()
+
+test_that("alt text errors", {
   # errors on null alt text
-  expect_error(access_img(ggplot(mpg, aes(displ, hwy)) + geom_point()),
-               "Please include alt text.")
+  expect_error(access_img(), "Please include alt text.")
   # errors if alt text set to empty string
-  expect_error(access_img(ggplot(mpg, aes(displ, hwy,)) + geom_point(),
-                          alt = ""), "Please include alt text.")
+  expect_error(access_img(alt = ""), "Please include alt text.")
   
 })
 
 test_that("success produces the required structure", {
   # wrapped in img tag
-  expect_match(as.character(access_img(img = ggplot(mpg, aes(displ, hwy,))
-                                       + geom_point(), alt = "some alt text")),
-               "^<img src=")
+  expect_match(as.character(access_img(alt = "some alt text")), "^<img src=")
   # alt is equivalent to user defined alt text
-  expect_match(as.character(access_img(img = ggplot(mpg, aes(displ, hwy,))
-                                       + geom_point(), alt = "some alt text")),
+  expect_match(as.character(access_img(alt = "some alt text")),
                'alt="some alt text"')
 
 })
