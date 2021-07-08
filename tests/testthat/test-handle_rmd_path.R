36aff201a2dca16d9b896d5abe8547de4f9b1bcd
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-08 13:52:20 +0100

    test: handle_rmd_path success outcome

--- tests/testthat/test-handle_rmd_path.R
+++ tests/testthat/test-handle_rmd_path.R
@@ -1,3 +1,5 @@
+
+# test fail ---------------------------------------------------------------
 test_that("Func fails as expected", {
   expect_error(handle_rmd_path(), "rmd_path is NULL.")
   
@@ -7,4 +9,20 @@ test_that("Func fails as expected", {
   expect_error(handle_rmd_path("something.Rmd"), "rmd file not found.")
 })
 
+# create an rmd to test ---------------------------------------------------
+# store the current wd in global scope and setwd to tempdir
+with(globalenv(), {.old_wd <- setwd(tempdir())})
+# create filename
+tmp <- tempfile(fileext = ".Rmd")
+# create file
+file.create(tmp, showWarnings = FALSE)
+writeLines("placeholder text", con = tmp)
 
+# test success ------------------------------------------------------------
+test_that("Expected output on success", {
+  expect_equal(handle_rmd_path(tmp), tmp)
+})
+
+
+# set the wd to test directory
+with(globalenv(), {setwd(.old_wd)})
