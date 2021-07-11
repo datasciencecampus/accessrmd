2b952bf96bf073bfe2042a8249ed3a66831f67db
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-11 07:40:11 +0100

    test: Message on no sus alt found

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(accessrmd)
 library(ggplot2)
+library(rlist)
 
 test_check("accessrmd")
--- tests/testthat/test-sus_alt.R
+++ tests/testthat/test-sus_alt.R
@@ -3,8 +3,16 @@ with(globalenv(), {.old_wd <- setwd(tempdir())})
 
 
 
+# dependencies ------------------------------------------------------------
 
+good_img <- tempfile(fileext = ".Rmd")
+file.create(good_img)
+writeLines("<img src='something' alt='something else'/>", con = good_img)
 
+# tests -------------------------------------------------------------------
+
+expect_message(sus_alt(good_img),
+               "No images with equal src and alt values found.")
 
 
 
