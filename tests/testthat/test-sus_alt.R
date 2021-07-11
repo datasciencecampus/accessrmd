34eb12e14f738827623e8d03e3f41b22fae3b9cc
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-11 08:04:59 +0100

    test: Warns on alt text equal to src

--- tests/testthat/test-sus_alt.R
+++ tests/testthat/test-sus_alt.R
@@ -9,6 +9,10 @@ good_img <- tempfile(fileext = ".Rmd")
 file.create(good_img)
 writeLines("<img src='something' alt='something else'/>", con = good_img)
 
+bad_img <- tempfile(fileext = ".Rmd")
+file.create(bad_img)
+writeLines("<img src='something' alt='something'/>", con = bad_img)
+
 # tests -------------------------------------------------------------------
 
 test_that("Messages when no suspicious cases are found", {
@@ -19,6 +23,14 @@ test_that("Messages when no suspicious cases are found", {
                  "No images with equal src and alt values found.")
 })
 
+test_that("Messages when dupe alt is found", {
+  expect_message(suppressWarnings(sus_alt(bad_img)),
+                 paste0("Checking ", bad_img, "..."))
+  expect_message(suppressWarnings(sus_alt(bad_img)),
+                 "No images with placeholder text found.")
+  expect_warning(sus_alt(bad_img), "alt text should not be equal to src.")
+  
+})
 
 
 
