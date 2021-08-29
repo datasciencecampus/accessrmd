8d2bfc33d060dc762c07c5bfba06e48f00191c50
r-leyshon <richard.leyshon@ons.gov.uk>
2021-08-29 20:01:56 +0100

    bug: Resolving tests depending on session info that do not pass on remote check

--- tests/testthat/test-access_rmd.R
+++ tests/testthat/test-access_rmd.R
@@ -4,6 +4,7 @@ with(globalenv(), {
 })
 # tests -------------------------------------------------------------------
 
+
 test_that("Func behaves as expected on minimal parameters", {
   expect_message(access_rmd("min imal", title = "minimal test", lan = "en"),
                  "Setting html lan to en")
@@ -24,9 +25,8 @@ test_that("Output has been written as expected", {
   expect_equal(grep("    <title>minimal test</title>", lines), 4)
   expect_equal(grep("    <h1 id=\"title toc-ignore\">minimal test</h1>" ,
                     lines), 5)
-  expect_equal(grep("    <h2 class=\"header_h2s\">richardleyshon</h2>", lines),
-               6)
-  expect_equal(grep("    <h2 class=\"header_h2s\">29 Aug 2021</h2>", lines), 7)
+  expect_equal(grep(Sys.info()[8], lines), 6)
+  expect_equal(grep(format(Sys.Date(), "%d %b %Y"), lines), 7)
   expect_equal(grep("  </header>", lines), 8)
   expect_equal(grep("  <body>", lines), 9)
 })
