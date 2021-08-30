53852db291960dd634fcdea78c70213e135c12c0
r-leyshon <richard.leyshon@ons.gov.uk>
2021-08-30 10:24:08 +0100

    feat: Adding language subtags to data

---
+++ R/langs.R
@@ -0,0 +1,11 @@
+#' Language subtag codes
+#'
+#' Subtag codes taken from the iana subtag registry.
+#'
+#' @docType data
+#'
+#' @usage data(langs)
+#' 
+#' @source \href{https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry}{IANA subtag registry}
+#' 
+"langs"
