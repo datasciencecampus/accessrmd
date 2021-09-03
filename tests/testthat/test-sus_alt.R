a4b31297709888db1e33c88df3984df29d250885
r-leyshon <richard.leyshon@ons.gov.uk>
2021-09-03 21:41:05 +0100

    tests: sus_alt tests require lang attribute

--- tests/testthat/test-sus_alt.R
+++ tests/testthat/test-sus_alt.R
@@ -9,22 +9,36 @@ with(globalenv(), {
 
 good_img <- tempfile(fileext = ".Rmd")
 file.create(good_img)
-writeLines("<img src='something' alt='something else'/>", con = good_img)
+writeLines(
+  "---
+lang: en
+---
+<img src='something' alt='something else'/>", con = good_img)
 
 bad_img <- tempfile(fileext = ".Rmd")
 file.create(bad_img)
-writeLines("<img src='something' alt='something'/>", con = bad_img)
+writeLines(
+  "---
+lang: en
+---
+<img src='something' alt='something'/>", con = bad_img)
 
 plac_img <- tempfile(fileext = ".Rmd")
 file.create(plac_img)
-writeLines("<img src='something' alt='nbsp'/>", con = plac_img)
+writeLines(
+  "---
+lang: en
+---
+<img src='something' alt='nbsp'/>", con = plac_img)
 
 specdim_imgs <- tempfile(fileext = ".Rmd")
 file.create(specdim_imgs)
 writeLines(
-  "
-  <img src='something' alt='acceptable alt text' height='300' width='300'/> 
-  <img src='something' alt='spacer' height='300' width='400'/>
+  "---
+lang: en
+---
+<img src='something' alt='acceptable alt text' height='300' width='300'/> 
+<img src='something' alt='spacer' height='300' width='400'/>
   ",
   con = specdim_imgs
 )
@@ -32,11 +46,14 @@ writeLines(
 mixed_imgs <- tempfile(fileext = ".Rmd")
 file.create(mixed_imgs)
 writeLines(
-  "<img src='unacceptable' alt='unacceptable' height='300' width='300'/> 
-  <img src='alt_is' alt='acceptable' height='300' width='300'/> 
-  <img src='unacceptable' alt='spacer' height='300' width='400'/>
-  ![acceptable](alt_is)
-  ![nbsp](unacceptable_alt)
+  "---
+lang: en
+---
+<img src='unacceptable' alt='unacceptable' height='300' width='300'/> 
+<img src='alt_is' alt='acceptable' height='300' width='300'/> 
+<img src='unacceptable' alt='spacer' height='300' width='400'/>
+![acceptable](alt_is)
+![nbsp](unacceptable_alt)
   ",
   con = mixed_imgs
 )
@@ -44,9 +61,12 @@ writeLines(
 missing_alt <- tempfile(fileext = ".Rmd")
 file.create(missing_alt)
 writeLines(
-  "<img src='no_alt_included'>
-  <img src='no_alt_included' alt=''>
-  ![](no_alt_included)
+  "---
+lang: en
+---
+<img src='no_alt_included'>
+<img src='no_alt_included' alt=''>
+![](no_alt_included)
   ",
   con = missing_alt
 )
@@ -79,7 +99,9 @@ test_that("Messages when dupe alt is found", {
     "No images with placeholder text found."
   )
   expect_warning(sus_alt(bad_img), "alt text should not be equal to src.")
-  expect_warning(sus_alt(bad_img), "1")
+  expect_warning(sus_alt(bad_img),
+                 "Check lines:
+ 4")
 })
 
 test_that("Messages when placeholder alt is found", {
@@ -95,11 +117,15 @@ test_that("Messages when placeholder alt is found", {
     sus_alt(plac_img),
     "alt text should not be empty or equal to 'spacer' or 'nbsp'."
   )
-  expect_warning(sus_alt(plac_img), "1")
+  expect_warning(sus_alt(plac_img),
+                 "Check lines:
+ 4")
 })
 
 test_that("imgs with square specified dims do not get flagged as suspicious", {
-  expect_warning(sus_alt(specdim_imgs), "3")
+  expect_warning(sus_alt(specdim_imgs),
+                 " Check lines:
+ 5")
 })
 
 
@@ -107,12 +133,12 @@ test_that("Complex case behaves as expected", {
   expect_warning(
     sus_alt(mixed_imgs),
     "Check lines:
- 3, 5"
+ 6, 8"
   )
   expect_warning(
     sus_alt(mixed_imgs),
     "Check lines:
- 1"
+ 4"
   )
 })
 
@@ -120,7 +146,7 @@ test_that("Blank alt is flagged", {
   expect_warning(
     sus_alt(missing_alt),
     " Check lines:
- 1, 2, 3"
+ 4, 5, 6"
   )
 })
 
