2fc2d433b9e9c34151ca90a1586f2cbcdbbb61df
r-leyshon <richard.leyshon@ons.gov.uk>
2021-09-03 20:28:03 +0100

    feat: Func retunrs NULL on lang with no associated alt len limit

--- R/find_alt_len.R
+++ R/find_alt_len.R
@@ -15,9 +15,9 @@ find_alt_len <- function(rmd_path = NULL, lan = detect_html_lang(lines)) {
   lims <- c(100, 115, 90)
   names(lims) <- c("en", "de", "ko")
   lines <- handle_rmd_path(rmd_path)
-  # compare aginst lims
+  # compare against lims
   lim_ind <- grep(lan, names(lims))
-  if(!is.null(lim_ind)){
+  if(length(lim_ind) > 0){
     return(unname(lims[lim_ind]))
   } else
     return(NULL)
---
+++ tests/testthat/test-find_alt_len.R
@@ -0,0 +1,117 @@
+with(globalenv(), {
+     .old_wd <- setwd(tempdir())})
+
+
+# deps --------------------------------------------------------------------
+
+en_file <- tempfile(fileext = ".Rmd")
+file.create(en_file)
+writeLines(
+  '---
+title: "Untitled"
+author: "Richard Leyshon"
+date: "12/07/2021"
+output:
+  html_document:
+    lang: "en"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown', con = en_file
+)
+
+de_file <- tempfile(fileext = ".Rmd")
+file.create(de_file)
+writeLines(
+  '---
+title: "Untitled"
+author: "Richard Leyshon"
+date: "12/07/2021"
+output:
+  html_document:
+    lang: "de"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown', con = de_file
+)
+
+ko_file <- tempfile(fileext = ".Rmd")
+file.create(ko_file)
+writeLines(
+  '---
+title: "Untitled"
+author: "Richard Leyshon"
+date: "12/07/2021"
+output:
+  html_document:
+    lang: "ko"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown', con = ko_file
+)
+
+us_file <- tempfile(fileext = ".Rmd")
+file.create(us_file)
+writeLines(
+  '---
+title: "Untitled"
+author: "Richard Leyshon"
+date: "12/07/2021"
+output:
+  html_document:
+    lang: "en-us"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown', con = us_file
+)
+
+cymru_file <- tempfile(fileext = ".Rmd")
+file.create(cymru_file)
+writeLines(
+  '---
+title: "test"
+author: "Richard Leyshon"
+date: "02/07/2021"
+output:
+  html_document:
+    lang: "cy"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown', con = cymru_file
+)
+
+# tests -------------------------------------------------------------------
+
+test_that("Func returns correct limits", {
+  expect_identical(find_alt_len(en_file), 100)
+  expect_identical(find_alt_len(de_file), 115)
+  expect_identical(find_alt_len(ko_file), 90)
+  expect_identical(find_alt_len(us_file), 100)
+  
+})
+
+test_that("Func returns NULL if no limit found", {
+  expect_null(find_alt_len(cymru_file))
+})
+
+with(globalenv(), {
+     setwd(.old_wd)})
