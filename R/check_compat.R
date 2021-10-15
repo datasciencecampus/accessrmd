3d1c27e0b150bea767e35385e4a473d64d9e9c72
r-leyshon <richard.leyshon@ons.gov.uk>
2021-10-15 06:30:37 +0100

    feat: Func tests compatibility of flavoured html outputs

---
+++ R/check_compat.R
@@ -0,0 +1,33 @@
+#' Checks if the html doctype is compatible.
+#'
+#' Checks to ensure file is not a special flavour of html output, including 
+#' xaringan, ioslides, flexdashboard or slidy. Output is an error if
+#' html doc is a special case.
+#'
+#' @param yaml_txt Output of 'readLines()' separated from body text.
+#'
+#' @return Error condition if non-standard html found.
+#'
+check_compat <- function(yaml_txt){
+  specials <- c("xaringan::moon_reader:", "ioslides_presentation",
+                "flexdashboard::flex_dashboard:", "slidy_presentation")
+  # Checking for flavoured html outputs first and warning on sig. accessibility
+  # errors at time of writing.
+  # extract pretty names for error messaging
+  names(specials) <- lapply(
+    str_split(string = specials, pattern = "_|::"), "[[", 1
+    )
+  # check all special cases and output error if found
+  for (outputs in specials) {
+    x <- grepl(pattern = outputs, yaml_txt)
+    if(x){
+      stop(paste(names(specials[grepl(outputs, specials)]),
+                 "output is not compatible. Non-trivial accessibility errors"))
+    }
+  }
+  # check for html output type. Stop if not.
+  html_loc <- grepl(pattern = "html_document", yaml_txt)
+  if (!html_loc){
+    stop("'access_head()' only works with html output.")
+    }
+}
