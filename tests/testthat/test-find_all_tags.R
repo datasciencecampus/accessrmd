3bee642d99d7520bd0a8cdae7dd46bb3aae7ebe3
r-leyshon <richard.leyshon@ons.gov.uk>
2021-10-16 08:23:00 +0100

    test: Tests for link outputs also

--- tests/testthat/test-find_all_tags.R
+++ tests/testthat/test-find_all_tags.R
@@ -3,7 +3,8 @@
 
 lines <- c(
   "<img src='something' alt='nbsp'/>", "![nbsp](something)",
-  "Not an img"
+  "Not an img", "[Here's a link](some_link.com)",
+  "<a href=\"https://some_link.com\">Here's another link</a>"
 )
 # tests -------------------------------------------------------------------
 
@@ -11,7 +12,11 @@ test_that("func finds imgs only", {
   # markdown & HTML imgs only
   expect_equal(length(find_all_tags(lines, tag = "img")), 2)
 })
+test_that("func finds links only", {
+  expect_equal(length(find_all_tags(lines, tag = "link")), 2)
+})
 
 test_that("func returns correct indices in names of out vec", {
   expect_equal(names(find_all_tags(lines, tag = "img")), c("1", "2"))
+  expect_equal(names(find_all_tags(lines, tag = "link")), c("4", "5"))
 })
