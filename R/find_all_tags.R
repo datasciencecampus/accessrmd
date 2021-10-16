2bca49b4dd87253563d8c4e1a8d705a223c0a0cc
r-leyshon <richard.leyshon@ons.gov.uk>
2021-10-16 07:35:14 +0100

    refactor: Renaming find_all_imgs to find_all_tags

--- R/find_all_tags.R
+++ R/find_all_tags.R
@@ -1,20 +1,26 @@
-#' Find any markdown or HTML syntax images within read lines.
+#' Find any markdown or HTML syntax tags within read lines.
 #'
-#' Check any lines for images and return the line numbers and values.
+#' Check any lines for images or links and return the line numbers and values.
 #'
 #' @param lines Read lines, default is NULL & typically used with output of
 #' handle_rmd_path.
+#' 
+#' @param tag The type of tag to be searched. Valid values are "img" or "link".
 #'
 #' @return A named vector with line numbers of any found images and their
 #' values.
-find_all_imgs <- function(lines = NULL) {
+#' 
+find_all_tags <- function(lines = NULL, tag = c("img", "link")) {
   # only want img tags but preserve indexing --------------------------------
   # set the names of the lines vector as a count
   names(lines) <- seq_along(lines)
+
   # regex matches img tags & ![]() markdown syntax
   # tested on https://regex101.com/r/CU4g3f/1
-  img_ind <- grep(pattern = "(<img.* *>|\\!\\[.* *]\\(.* *\\))", lines)
-  img_only <- lines[img_ind]
-
-  return(img_only)
+  tag_ind <- grep(pattern = if(tag == "img"){
+    "(<img.* *>|\\!\\[.* *]\\(.* *\\))"
+    }, lines)
+  
+  tag_only <- lines[tag_ind]
+  return(tag_only)
 }
