92c6672b5720ff14ab8dcc3154188168a30d98b9
r-leyshon <richard.leyshon@ons.gov.uk>
2021-10-16 07:41:21 +0100

    test: Updating test for new param

--- tests/testthat/test-find_all_tags.R
+++ tests/testthat/test-find_all_tags.R
@@ -9,9 +9,9 @@ lines <- c(
 
 test_that("func finds imgs only", {
   # markdown & HTML imgs only
-  expect_equal(length(find_all_imgs(lines)), 2)
+  expect_equal(length(find_all_tags(lines, tag = "img")), 2)
 })
 
 test_that("func returns correct indices in names of out vec", {
-  expect_equal(names(find_all_imgs(lines)), c("1", "2"))
+  expect_equal(names(find_all_tags(lines, tag = "img")), c("1", "2"))
 })
