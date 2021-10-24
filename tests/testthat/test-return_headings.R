792bf754ea0ba10495ed822f2750ad27eb33ce3f
r-leyshon <richard.leyshon@ons.gov.uk>
2021-10-24 19:51:59 +0100

    test: Func returns NULL if length zero character vector

--- tests/testthat/test-return_headings.R
+++ tests/testthat/test-return_headings.R
@@ -15,3 +15,7 @@ test_that("lvl results in correct heading tag",
 test_that("Correct class returned", 
           expect_true(grepl("title toc-ignore", unlist(eg_title))[2])
           )
+test_that("Returns NULL on length zero character",
+          expect_null(
+            return_heading(txt = character(), lvl = 1, class = "title"))
+          )
