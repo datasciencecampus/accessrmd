c3b7843ca65ffc7c12e739121c7b26bbfe264f53
r-leyshon <richard.leyshon@ons.gov.uk>
2021-11-08 06:48:08 +0000

    test: Func returns found themes

--- tests/testthat/test-find_theme.R
+++ tests/testthat/test-find_theme.R
@@ -1,10 +1,45 @@
 
 # deps --------------------------------------------------------------------
 
+cerulean <- c('---',
+'title: "cerulean"',
+'author: "R Leyshon"',
+'date: "08/11/2021"',
+'output:',
+'    html_document:',
+'      theme: cerulean',
+'---',
+'',
+'```{r setup, include=FALSE}',
+'knitr::opts_chunk$set(echo = TRUE)',
+'```',
+'',
+'## R Markdown')
 
+flatly <- c('---',
+'title: "flatly"',
+'author: "R Leyshon"',
+'date: "08/11/2021"',
+'output:',
+'    html_document:',
+'      theme: flatly',
+'---',
+'',
+'```{r setup, include=FALSE}',
+'knitr::opts_chunk$set(echo = TRUE)',
+'```',
+'',
+'## R Markdown')
 
 
 
 # tests -------------------------------------------------------------------
+test_that("Func returns found themes", {
+  expect_identical(find_theme(cerulean), "cerulean")
+  expect_identical(find_theme(flatly), "flatly")
+  
+})
+
+
 
 
