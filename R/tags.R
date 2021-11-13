9b3fa3c7b67562aa7a4c5e35cce95b38f51149c7
r-leyshon <richard.leyshon@ons.gov.uk>
2021-11-13 07:00:00 +0000

    chore: Adding gpl license and url to tags namespace

--- R/tags.R
+++ R/tags.R
@@ -1,4 +1,6 @@
 #' "tags" object from "htmltools".
+#' License: GPL (>= 2)
+#' URL: https://github.com/rstudio/htmltools
 #'
 #' Does not appear to be exported from htmltools namespace. Dependency has been
 #' established with "utils::getFromNamespace"
