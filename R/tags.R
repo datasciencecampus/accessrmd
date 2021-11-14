0a33696e4d4205764e5d4b4f3df151a720bcf071
r-leyshon <richard.leyshon@ons.gov.uk>
2021-11-14 07:43:35 +0000

    chore: Adjusting htmltools::tags dep method as suggested by CS, rstudio

--- R/tags.R
+++ R/tags.R
@@ -1,13 +1,10 @@
 #' "tags" object from "htmltools".
 #' License: GPL (>= 2)
 #' [htmltools GitHub](https://github.com/rstudio/htmltools)
-#' [gpl 2 terms](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
-#' 
-#'
-#' Does not appear to be exported from htmltools namespace. Dependency has been
-#' established with "utils::getFromNamespace"
-#'
-#' @return The html "tags" from "htmltools".
-#'
-#' @importFrom utils getFromNamespace
-tags <- getFromNamespace("tags", "htmltools")
+#' @name tags
+#' @rdname tags
+#' @keywords internal
+#' @export
+#' @importFrom htmltools tags
+#' @return Wraps provided paramaters in HTML tag specified.
+globalVariables("tags")
