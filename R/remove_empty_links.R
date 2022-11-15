b8554c23d1b7324a8eb781977c3054d235133a4c
Jake Marshall <jake.marshall@ons.gov.uk>
2022-11-15 18:19:38 +0000

    remove_empty_links function created.

--- R/remove_empty_links.R
+++ R/remove_empty_links.R
@@ -1,34 +1,40 @@
-#' Removes the empty links attached to each code cell in the HTML output.
-#'
-#' Reads an Rmd file, searching for the following structure in the HTML:
+#' Reads an HTML file, searching for the following structure in the HTML:
 #' 
 #' <a href="#some_pattern" aria-hidden="true" tabindex="-1"></a>
 #' 
 #' and tweak it to remove the href = "#some_pattern" component. 
+#' 
+#' The file is written out to a new HTML, with the prefix "accessible" 
+#' 
+#' Since windows recognises the \ character as an escape one, the inclusion of windows = TRUE allows
+#' the user to specify if this is the case and as such the slashes will be replaced with double slashes
+#'(\\) to circumvent this problem. 
 #'
 #' @param html_file Path to the Rmd that requires links to be checked. Rmd must
 #' be output type html.
-#'
-#' @return Lines of any urls that respond with an error.
-#'
-#' @importFrom stringr str_extract
 #' 
-#' @export
+#' @windows A boolean that denotes whether to correct the windows backslash character in filepaths
+#' 
+#' @return A message displaying whether there are any empty links remaining or not.
+#'
+#' @importFrom stringr str_remove
+#' 
+#' @export A new HTML file with the prefix 'accessible_'
 
 
-remove_empty_links <- function(html_file) {
+remove_empty_links <- function(html_file, windows = TRUE) {
   message(paste("Fixing", basename(html_file)))
   lines <- readLines(html_file)
   # Find the code cells only 
-  code_cells <- find_all_code_cells(lines = lines) # Write this function
+  # code_cells <- find_all_code_cells(lines = lines) # Write this function
   # Remove the empty links
-  cleaned_code_cells <- gsub('href="#cb[0-9]*-[0-9]*" aria-hidden="true" tabindex="-1"', 
+  cleaned_lines <- gsub('href="#cb[0-9]*-[0-9]*" aria-hidden="true" tabindex="-1"', 
                              'aria-hidden="true" tabindex="-1"',
-                             code_cells)
+                             lines)
   
   # Compute number of empty links remaining
   errors <- sum(grepl(pattern = 'href="#cb[0-9]*-[0-9]*" aria-hidden="true" tabindex="-1"', 
-                      cleaned_code_cells))
+                      cleaned_lines))
   
   # Output messages
   if (errors == 0) {
@@ -37,4 +43,27 @@ remove_empty_links <- function(html_file) {
   else {
     warning("There are still some empty links, diagnose in the HTML and add this to the issues page at https://github.com/datasciencecampus/accessrmd")
   }
+  
+  # Reassemble the document
+  
+  # outfile should be placed in accessrmd folder
+  # Replace single slashes with double slashes to solve a windows issue
+  if (windows == TRUE){
+    html_path <- gsub("\\", "\\\\", html_file, fixed = TRUE)
+  }
+  else {
+    html_path <- html_file
+  }
+  
+  html_name <- basename(html_path)
+  # get the directory
+  html_dir <- str_remove(html_path, html_name)
+  # append accessrmd
+  outfile <- paste0(html_dir, "accessible_", html_name)
+
+  message(paste("Writing file to", outfile))
+  return(writeLines(paste(cleaned_lines), con = outfile))
+
 }
+
+
