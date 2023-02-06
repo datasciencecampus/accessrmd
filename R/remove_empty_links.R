98284719a86e298b10787063d49bb5450d37c62f
Jake Marshall <jake.marshall@ons.gov.uk>
2023-02-06 10:40:32 +0000

    Cleaned up remove_empty_links()

--- R/remove_empty_links.R
+++ R/remove_empty_links.R
@@ -4,29 +4,30 @@
 #' 
 #' and tweak it to remove the href = "#some_pattern" component. 
 #' 
-#' The file is written out to a new HTML, with the prefix "accessible" 
+#' These must be removed as they are causing code cells to be skipped by screen readers
+#' and are one of the biggest Web Content Accessibility Guideline Errors picked up on
+#' during the audit. 
 #' 
-#' Since windows recognises the \ character as an escape one, the inclusion of windows = TRUE allows
-#' the user to specify if this is the case and as such the slashes will be replaced with double slashes
-#'(\\) to circumvent this problem. 
+#' The file is written out to a new HTML, with the prefix "accessible" 
 #'
 #' @param html_file Path to the Rmd that requires links to be checked. Rmd must
 #' be output type html.
 #' 
-#' @windows A boolean that denotes whether to correct the windows backslash character in filepaths
-#' 
-#' @return A message displaying whether there are any empty links remaining or not.
+#' @return A message displaying whether there are any empty links remaining or not and
+#' a further message detailing the creation of the new accessible html, displaying its
+#' absolute file path.
 #'
 #' @importFrom stringr str_remove
 #' 
 #' @export A new HTML file with the prefix 'accessible_'
 
 
-remove_empty_links <- function(html_file, windows = TRUE) {
+remove_empty_links <- function(html_file) {
+  
+  # Initial message
   message(paste("Fixing", basename(html_file)))
   lines <- readLines(html_file)
-  # Find the code cells only 
-  # code_cells <- find_all_code_cells(lines = lines) # Write this function
+  
   # Remove the empty links
   cleaned_lines <- gsub('href="#cb[0-9]*-[0-9]*" aria-hidden="true" tabindex="-1"', 
                              'aria-hidden="true" tabindex="-1"',
@@ -46,19 +47,10 @@ remove_empty_links <- function(html_file, windows = TRUE) {
   
   # Reassemble the document
   
-  # outfile should be placed in accessrmd folder
-  # Replace single slashes with double slashes to solve a windows issue
-  if (windows == TRUE){
-    html_path <- gsub("\\", "\\\\", html_file, fixed = TRUE)
-  }
-  else {
-    html_path <- html_file
-  }
-  
-  html_name <- basename(html_path)
+  html_name <- basename(html_file)
   # get the directory
-  html_dir <- str_remove(html_path, html_name)
-  # append accessrmd
+  html_dir <- str_remove(html_file, html_name)
+  # append accessible_
   outfile <- paste0(html_dir, "accessible_", html_name)
 
   message(paste("Writing file to", outfile))
